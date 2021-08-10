66a7e251276643e265e4728e299130cfdb67c9b0
HEUMMAN <whdudgma123@kpu.ac.kr>
2021-08-10 14:24:49 +0900

    homework_4
    homework_4

--- sparta/week_4/homework/02_get_count_of_departments_cleaned_by_robot_vaccum.py
+++ sparta/week_4/homework/02_get_count_of_departments_cleaned_by_robot_vaccum.py
@@ -13,6 +13,33 @@ current_room_map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 def get_count_of_departments_cleaned_by_robot_vacuum(r, c, d, room_map):
-    return
+    #0:북, 1:동, 2:남, 3:서
+    dy = [-1, 0, 1, 0]
+    dx = [0, 1, 0, -1]
+    cnt = 1
+    room_map[r][c] = -1
+    queue = [[r, c, d]]
+
+    while queue:
+        r, c, d = queue.pop(0)
+        temp_d = d
+
+        for i in range(4):
+            temp_d = (temp_d + 3) % 4
+            new_r, new_c = r + dy[temp_d], c + dx[temp_d]
+
+            if 0 <= new_r < len(room_map) and 0 <= new_c < len(room_map[0]) and room_map[new_r][new_c] == 0:
+                cnt += 1
+                room_map[new_r][new_c] = -1
+                queue.append([new_r, new_c, temp_d])
+                break
+
+            elif i == 3:
+                new_r, new_c = r + dy[(d + 2) % 4], c + dx[(d + 2) % 4]
+                queue.append([new_r, new_c, d])
+
+                if room_map[new_r][new_c] == 1:
+                    return cnt
+
 # 57 가 출력되어야 합니다!
 print(get_count_of_departments_cleaned_by_robot_vacuum(current_r, current_c, current_d, current_room_map))
--- sparta/week_4/homework/03_get_all_ways_of_theater_seat.py
+++ sparta/week_4/homework/03_get_all_ways_of_theater_seat.py
@@ -1,9 +1,36 @@
-seat_count = 9
-vip_seat_array = [4, 7]
+seat_count = 11
+vip_seat_array = [2 ,5]
+fibo_memo = {
+    1: 1,
+    2: 2
+}
+
+
+def fibo(n, fibo_memo):
+    if n in fibo_memo:
+        return fibo_memo[n]
+    fibo_memo[n] = fibo(n-1, fibo_memo) + fibo(n-2, fibo_memo)
+    return fibo_memo[n]
 
 
 def get_all_ways_of_theater_seat(total_count, fixed_seat_array):
-    return
+    result = []
+    end = 0
+    for i in range(1, seat_count+1):
+        end += 1
+        if i in vip_seat_array:
+            result.append(end-1)
+            end = 0
+
+        elif i == seat_count:
+            result.append(end)
+            end = 0
+    answer = 1
+    for i in range(len(result)):
+        answer *= fibo(result[i], fibo_memo)
+
+    print(result)
+    return answer
 
 
 # 12가 출력되어야 합니다!
