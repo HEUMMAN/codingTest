d83d2bdc5329e61a1d2253d52aacf436eaa00946
HEUMMAN <69341986+HEUMMAN@users.noreply.github.com>
2021-08-27 23:42:16 +0900

    8/27

---
+++ programmers/bfs_dfs/단어 변환.py
@@ -0,0 +1,19 @@
+def solution(begin, target, words):
+    answer = 0
+    if target not in words:
+        return 0
+    visited = []
+    visited.append((begin, 0))
+    while visited:
+        now, depth = visited.pop(0)
+        for word in words:
+            diff = 0
+            for i in range(len(word)):
+                if now[i] != word[i]:
+                    diff += 1
+            if diff == 1 and word == target:
+                depth += 1
+                return depth
+            elif diff == 1:
+                visited.append((word, depth + 1))
+    return answer
