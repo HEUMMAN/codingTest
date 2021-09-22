14feab7bc172a2625adc4ae14ddd09bfdbb64bc7
HEUMMAN <69341986+HEUMMAN@users.noreply.github.com>
2021-09-22 20:46:12 +0900

    9/22

---
+++ programmers/이분탐색/징검다리.py
@@ -0,0 +1,26 @@
+def solution(distance, rocks, n):
+    rocks.sort()
+    rocks.append(distance)
+    left, right = 0, distance
+    answer = 0
+
+    while left <= right:
+        prev = 0
+        mins = 1000000000
+        removed_rocks = 0
+        mid = (left + right) // 2
+
+        for i in range(len(rocks)):
+            min_rock_distance = rocks[i] - prev
+            if min_rock_distance < mid:
+                if i != len(rocks) - 1:
+                    removed_rocks += 1
+            else:
+                mins = min(mins, min_rock_distance)
+                prev = rocks[i]
+            if removed_rocks > n:
+                right = mid - 1
+            else:
+                answer = mins
+                left = mid + 1
+    return answer
